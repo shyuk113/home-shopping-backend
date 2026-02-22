ec1bf6a9c782c1c94d5d8b710a45af5ba463a146
이성혁 <danial2569@gmail.com>
2026-02-22 22:01:45 +0900

    feat(member): 멤버 엔티티 생성자 추가

---
+++ shop-backend/shop-backend/src/main/java/com/shop/backend/Member/domain/model/Member.java
@@ -0,0 +1,42 @@
+package com.shop.backend.Member.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+    private String email;
+
+    @Column(length = 100)
+    private String address;
+
+    @Builder
+    public Member(String name, String phone, String email, String address){
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+
+}
