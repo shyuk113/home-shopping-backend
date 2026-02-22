aa67e83571d731a68bf2b537849d28de386f1fa2
이성혁 <danial2569@gmail.com>
2026-02-22 23:44:23 +0900

    feat(auth): JwtProvider 클래스 구현

---
+++ shop-backend/shop-backend/src/main/java/com/shop/backend/global/jwt/JwtProvider.java
@@ -0,0 +1,53 @@
+package com.shop.backend.global.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final long accessTokenValidity = 1000 * 60 * 30; //30 분
+
+    public String createAccessToken(String email, String role){
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + accessTokenValidity);
+
+        return Jwts.builder()
+            .setSubject(email)
+            .claim("role",role)
+            .setIssuedAt(now)
+            .setExpiration(expiry)
+            .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
+            .compact();
+    }
+
+    public String getEmail(String token){
+        return Jwts.parserBuilder()
+            .setSigningKey(secretKey.getBytes())
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                .setSigningKey(secretKey.getBytes())
+                .build()
+                .parseClaimsJws(token);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+}
