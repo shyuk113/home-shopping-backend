430867e49442fc9db760448e48a0eed2bb61d763
이성혁 <danial2569@gmail.com>
2026-02-22 22:02:15 +0900

    feat(order): 주문 CRUD API 추가

---
+++ shop-backend/shop-backend/src/main/java/com/shop/backend/Order/domain/model/OrderItem.java
@@ -0,0 +1,58 @@
+package com.shop.backend.Order.domain.model;
+
+import com.shop.backend.Item.domain.model.Item;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.transaction.annotation.Transactional;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int orderPrice;
+
+    private int quantity;
+
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int quantity){
+        OrderItem orderItem = new OrderItem();
+        orderItem.item = item;
+        orderItem.orderPrice = orderPrice;
+        orderItem.quantity = quantity;
+
+        orderItem.setItem(item);
+        item.removeStock(quantity);
+
+        return orderItem;
+    }
+
+    public void cancel(){
+        getItem().addStock(quantity);
+    }
+
+    public int getTotalPrice(){
+        return getOrderPrice() * getQuantity();
+    }
+}
+
---
+++ shop-backend/shop-backend/src/main/java/com/shop/backend/Order/domain/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.shop.backend.Order.domain.repository;
+
+import com.shop.backend.Order.domain.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
