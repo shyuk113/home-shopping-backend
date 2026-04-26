ab3e21ed4b5d71492add57a9fc12720d4807be3c
이성혁 <danial2569@gmail.com>
2026-04-26 14:02:40 +0900

    refactor(application): 불필요 import 제거, EnableJpaAuditing 애노테이션 추가

--- shop-backend/shop-backend/src/main/java/com/shop/backend/ShopBackendApplication.java
+++ shop-backend/shop-backend/src/main/java/com/shop/backend/ShopBackendApplication.java
@@ -3,13 +3,14 @@ package com.shop.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableCaching
 @SpringBootApplication
+@EnableJpaAuditing
 public class ShopBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopBackendApplication.class, args);
 	}
-
 }
