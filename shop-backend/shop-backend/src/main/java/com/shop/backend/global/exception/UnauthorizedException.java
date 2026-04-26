ffb728302c5a0f48b17a3a22ce0a6baeecc48d96
이성혁 <danial2569@gmail.com>
2026-04-26 11:57:32 +0900

    feat(exception): 권한 없는 이용자에 대한 예외 처리

---
+++ shop-backend/shop-backend/src/main/java/com/shop/backend/global/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.shop.backend.global.exception;
+
+public class UnauthorizedException extends RuntimeException{
+
+    public UnauthorizedException(String message){
+        super(message);
+    }
+
+}
