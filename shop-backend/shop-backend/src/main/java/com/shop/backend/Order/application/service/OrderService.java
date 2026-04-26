7882915cbdd2c859b5607902f96d70b8039b90ca
이성혁 <danial2569@gmail.com>
2026-04-26 12:01:31 +0900

    feat(item): 재고 감소에 대한 동시성 처리
    -findByIdWithLock 추가 (race  condition 해결)

--- shop-backend/shop-backend/src/main/java/com/shop/backend/Order/application/service/OrderService.java
+++ shop-backend/shop-backend/src/main/java/com/shop/backend/Order/application/service/OrderService.java
@@ -7,7 +7,9 @@ import com.shop.backend.Member.domain.model.Member;
 import com.shop.backend.Member.domain.repository.MemberRepository;
 import com.shop.backend.Order.domain.model.Order;
 import com.shop.backend.Order.domain.model.OrderItem;
+import com.shop.backend.Order.domain.model.OrderStatus;
 import com.shop.backend.Order.domain.repository.OrderRepository;
+import com.shop.backend.Order.presentation.dto.response.OrderResponse;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +28,7 @@ public class OrderService {
     @Transactional
     public Long order(Long memberId, Long itemId, int quantity){
         Member member = memberRepository.findById(memberId).orElseThrow(()-> new EntityNotFoundException("Member not found"));
-        Item item = itemRepository.findById(itemId).orElseThrow(()-> new EntityNotFoundException("Item not found"));
+        Item item = itemRepository.findByIdWithLock(itemId).orElseThrow(()-> new EntityNotFoundException("Item not found"));
 
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), quantity);
 
@@ -35,5 +37,32 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    @Transactional(readOnly = true) //회원의 전체 주문 목록
+    public List<OrderResponse> getOrders(Long memberId){
+        return orderRepository.findByMemberId(memberId).stream()
+            .map(OrderResponse::from)
+            .toList();
+    }
+
+    @Transactional(readOnly = true) // 단건 조회
+    public OrderResponse getOrder(Long orderId){
+        Order order = orderRepository.findById(orderId)
+            .orElseThrow(() -> new EntityNotFoundException("Order not found"));
+        return OrderResponse.from(order);
+    }
+
+    @Transactional // 주문 취소
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findById(orderId)
+            .orElseThrow(()-> new EntityNotFoundException("Order not found"));
+
+        if(order.getStatus() == OrderStatus.CANCEL){
+            throw new IllegalStateException("이미 취소된 주문입니다.");
+        }
+
+        order.setStatus(OrderStatus.CANCEL);
+        order.getOrderItems().forEach(OrderItem::cancel);
     }
 }
